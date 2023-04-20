d40e81633ec6c028557a82fb82d14a8f0ce4f0cc
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-04-20 15:23:42 -0300

    Adding debugging try except in speech-to-text.py.

--- python/speech-to-text.py
+++ python/speech-to-text.py
@@ -8,7 +8,10 @@ openai.api_key = os.getenv('OPENAI_SECRET_KEY')
 
 audio_file_name = input()
 audio_file_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'javascript', 'uploads', audio_file_name))
-audio_file = open(audio_file_path, 'rb')
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
+try:
+    audio_file = open(audio_file_path, 'rb')
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+except:
+    transcript = {'text':'FAILED - path: ' + audio_file_path}
 sys.stdout.write(transcript.text)
 sys.stdout.flush()
