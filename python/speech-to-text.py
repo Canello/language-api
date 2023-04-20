f742ea877ffad925a4ca70f954d14a5c42e31a30
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-04-20 15:34:25 -0300

    Debbuging speech-to-text.py.

--- python/speech-to-text.py
+++ python/speech-to-text.py
@@ -8,10 +8,10 @@ openai.api_key = os.getenv('OPENAI_SECRET_KEY')
 
 audio_file_name = input()
 audio_file_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'javascript', 'uploads', audio_file_name))
-try:
-    audio_file = open(audio_file_path, 'rb')
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-except:
-    transcript = {'text':'FAILED - path: ' + audio_file_path}
+# try:
+#     audio_file = open(audio_file_path, 'rb')
+#     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+# except:
+transcript = {'text':'FAILED - path: ' + audio_file_path}
 sys.stdout.write(transcript.text)
 sys.stdout.flush()
