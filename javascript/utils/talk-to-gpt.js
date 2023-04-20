d57c8f6ca51de040e91d925b0bbb5b6e127dae18
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-04-20 13:29:08 -0300

    Fixed file paths.

--- javascript/utils/talk-to-gpt.js
+++ javascript/utils/talk-to-gpt.js
@@ -1,12 +1,24 @@
+const path = require("path");
 const { runPythonScript } = require("./run-python-script");
 
-// Remember to fix this file paths
 const talkToGPT = async (messages) => {
-    const pythonScriptPath =
-        "C:/Users/gughi/documents/projects/language-js/language-api/python/talk-to-gpt.py";
+    const pythonScriptPath = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "python",
+        "talk-to-gpt.py"
+    );
     const args = [JSON.stringify(messages)];
-    const envActivatePath =
-        "C:/Users/gughi/documents/projects/language-js/language-api/python/language-api/scripts/activate";
+    const envActivatePath = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "python",
+        "language-api",
+        process.env.ENV_ACTIVATION_FOLDER,
+        "activate"
+    );
     return await runPythonScript(pythonScriptPath, args, envActivatePath);
 };
 
--- python/speech-to-text.py
+++ python/speech-to-text.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_SECRET_KEY')
 
-audio_file_path = 'C:/Users/gughi/documents/projects/language-js/language-api/javascript/uploads/audio.wav'
+audio_file_path = os.path.abspath(os.path.join(os.getcwd(), '..','javascript','uploads','audio.wav'))
 audio_file = open(audio_file_path, 'rb')
 transcript = openai.Audio.transcribe("whisper-1", audio_file)
 sys.stdout.write(transcript.text)
